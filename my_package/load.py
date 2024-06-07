72a7028214b4b0ffe025647706afec604d8d2fd5
Yubin (Harvey) <yubinhu@126.com>
2024-06-07 11:59:31 -0700

    Add financial news loader

--- my_package/load.py
+++ my_package/load.py
@@ -1,6 +1,8 @@
 # my_package/load.py
 from typing import List
 import pandas as pd
+import json
+import os
 from .data_models import Document
 
 class BaseLoader:
@@ -17,4 +19,24 @@ class FootballNewsLoader(BaseLoader):
             Document(id=str(i), dataset='football', title=row['title'], text=row['content'])
             for i, row in df.iterrows()
         ]
+        return documents
+
+class FinancialNewsLoader(BaseLoader):
+    def __init__(self, directory_path: str):
+        self.directory_path = directory_path
+
+    def load(self) -> List[Document]:
+        documents = []
+        for filename in os.listdir(self.directory_path):
+            if filename.endswith(".json"):
+                file_path = os.path.join(self.directory_path, filename)
+                with open(file_path, 'r') as file:
+                    data = json.load(file)
+                    document = Document(
+                        id=data['uuid'],
+                        dataset='financial',
+                        title=data['title'],
+                        text=data['text']
+                    )
+                    documents.append(document)
         return documents
